9cbe04cef60aaef5a6712ba63a000b8d8cb23232
chetan-90 <133109083+chetan-90@users.noreply.github.com>
2024-06-21 23:12:34 +0530

    env file updates

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,5 +1,5 @@
-// const backendDomain =   "http://localhost:8080" 
-const backendDomain =  process.env.REACT_APP_BACKEND_URL  
+const backendDomain =   "http://localhost:8080" 
+// const backendDomain =  process.env.REACT_APP_BACKEND_URL  || "http://localhost:8080"
 
 console.log("process.env.REACT_APP_BACKEND_URL  ", process.env.REACT_APP_BACKEND_URL );
 
