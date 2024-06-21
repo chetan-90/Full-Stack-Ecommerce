1abdab0595130ef8aaac40aee1ccad7e2a3b0ee6
chetan-90 <133109083+chetan-90@users.noreply.github.com>
2024-06-21 19:24:05 +0530

    backend url

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomain =  process.env.REACT_APP_BACKEND_URL //"http://localhost:8080"
+const backendDomain = process.env.REACT_APP_BACKEND_URL  // "http://localhost:8080"
 
 const SummaryApi ={
     signUp : {
