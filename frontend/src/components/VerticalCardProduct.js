1f6e261b9d5aa32254a8695f4855206a51b167c1
chetan-90 <133109083+chetan-90@users.noreply.github.com>
2024-08-28 22:18:56 +0530

    file

--- frontend/src/components/VerticalCardProduct.js
+++ frontend/src/components/VerticalCardProduct.js
@@ -1,12 +1,18 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import scrollTop from '../helpers/scrollTop'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import fetchCategoryWiseProduct from '../helpers/fetchCategoryWiseProduct'
 import displayINRCurrency from '../helpers/displayCurrency'
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6'
+import { Link } from 'react-router-dom'
 import Context from '../context'
 import addToCart from '../helpers/addToCart'
 
-const VerticalCard = ({loading,data=[]}) => {
+const VerticalCardProduct = ({category, heading}) => {
+    const [data,setData] = useState([])
+    const [loading,setLoading] = useState(true)
     const loadingList = new Array(13).fill(null)
+
+    const [scroll,setScroll] = useState(0)
+    const scrollElement = useRef()
 
     const { fetchUserAddToCart } = useContext(Context)
 
@@ -14,9 +20,39 @@ const VerticalCard = ({loading,data=[]}) => {
         await addToCart(e,id)
        fetchUserAddToCart()
     }
-  return (
-    <div className='grid grid-cols-[repeat(auto-fit,minmax(260px,300px))] justify-center md:justify-between md:gap-4 overflow-x-scroll scrollbar-none transition-all' >
 
+    const fetchData = async() =>{
+        setLoading(true)
+        const categoryProduct = await fetchCategoryWiseProduct(category)
+        setLoading(false)
+        console.log("categoryProduct",categoryProduct);
+        
+
+        setData(categoryProduct?.data)
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    const scrollRight = () =>{
+        scrollElement.current.scrollLeft += 300
+    }
+    const scrollLeft = () =>{
+        scrollElement.current.scrollLeft -= 300
+    }
+
+
+  return (
+    <div className='container mx-auto px-4 my-6 relative'>
+
+            <h2 className='text-2xl font-semibold py-4'>{heading}</h2>
+
+                
+           <div className='flex items-center gap-4 md:gap-6 overflow-x-scroll scrollbar-none transition-all' ref={scrollElement}>
+
+            <button  className='bg-white shadow-md rounded-full p-1 absolute left-0 text-lg hidden md:block' onClick={scrollLeft}><FaAngleLeft/></button>
+            <button  className='bg-white shadow-md rounded-full p-1 absolute right-0 text-lg hidden md:block' onClick={scrollRight}><FaAngleRight/></button> 
 
            {
 
@@ -41,9 +77,9 @@ const VerticalCard = ({loading,data=[]}) => {
                 ) : (
                     data.map((product,index)=>{
                         return(
-                            <Link to={"/product/"+product?._id} className='w-full min-w-[280px]  md:min-w-[300px] max-w-[280px] md:max-w-[300px]  bg-white rounded-sm shadow ' onClick={scrollTop}>
+                            <Link to={"product/"+product?._id} className='w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow '>
                                 <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
-                                    <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply' alt='productImage'/>
+                                    <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply' alt='imag'/>
                                 </div>
                                 <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black'>{product?.productName}</h2>
@@ -61,7 +97,10 @@ const VerticalCard = ({loading,data=[]}) => {
                 
             }
            </div>
+            
+
+    </div>
   )
 }
 
-export default VerticalCard
+export default VerticalCardProduct
