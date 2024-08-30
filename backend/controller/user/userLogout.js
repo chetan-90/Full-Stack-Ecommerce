8bd2de97abff70e404736da161361b0d9e073049
chetan-90 <133109083+chetan-90@users.noreply.github.com>
2024-08-30 08:51:29 +0530

    token verify

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -8,6 +8,7 @@ async function userLogout(req, res){
           
 
         res.clearCookie("token",tokenOption)
+        console.log("token verify",res.getHeaders());
 
         res.json({
             message : "Logged out successfully",
